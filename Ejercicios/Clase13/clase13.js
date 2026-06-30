b4cd328d457ed144ae92ed3d93dbbe8518286ced
maykel0 <maykel54547564844@hotmail.com>
2026-06-30 11:28:53 -0600

    actualizacion clase13.js nuevo ejemplo

--- Ejercicios/Clase13/clase13.js
+++ Ejercicios/Clase13/clase13.js
@@ -53,3 +53,20 @@ perro.comer();
 
 console.log(perro.__proto__ === animal);
 */
+
+class CuentaBancaria {
+    constructor(saldoInicial){
+        this._saldo = saldoInicial;
+    }
+    get saldo(){
+        return this._saldo;
+    }
+    set saldo(valor){
+        if(valor < 0) throw new Error("Saldo no puede ser negativo");
+        this._saldo = valor; 
+    }
+}
+
+const cuenta = new CuentaBancaria(1000);
+cuenta.saldo = 1500;
+console.log(cuenta.saldo);
